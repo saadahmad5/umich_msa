6b590630e2a2b5850dfbd855c00cee3d589a1e2d
Saad Ahmad <saadtahir5@gmail.com>
2021-12-26 22:18:42 -0500

    worked on events on calender dummy

---
+++ lib/models/event.dart
@@ -0,0 +1,10 @@
+class Event {
+  late String title;
+  late String description;
+  late DateTime dateTime;
+  late String imageUrl;
+  late String roomInfo;
+  late String address;
+
+  Event(this.title);
+}
--- lib/screens/widgets/events_widget.dart
+++ lib/screens/widgets/events_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:umich_msa/models/event.dart';
 
 class EventsWidget extends StatefulWidget {
   const EventsWidget({Key? key}) : super(key: key);
@@ -10,11 +12,17 @@ class EventsWidget extends StatefulWidget {
 
 class _EventsWidgetState extends State<EventsWidget> {
   late CalendarController _calendarController;
+  late TextEditingController _eventController;
+  late Map<DateTime, List<Event>> events;
+  late List<dynamic> selectedEvents;
 
   @override
   void initState() {
     super.initState();
     _calendarController = CalendarController();
+    _eventController = TextEditingController();
+    events = {};
+    selectedEvents = [];
   }
 
   @override
@@ -25,15 +33,132 @@ class _EventsWidgetState extends State<EventsWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return TableCalendar(
-      startDay: DateTime(
-          DateTime.now().year, DateTime.now().month - 3, DateTime.now().day),
-      endDay: DateTime(
-          DateTime.now().year, DateTime.now().month + 3, DateTime.now().day),
-      initialSelectedDay: DateTime.now(),
-      initialCalendarFormat: CalendarFormat.twoWeeks,
-      calendarController: _calendarController, //required
-      locale: 'en_US',
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          FloatingActionButton(
+            backgroundColor: Colors.black,
+            child: Icon(Icons.add),
+            onPressed: _showAddDialog,
+          ),
+          TableCalendar(
+            events: events,
+            startDay: DateTime(DateTime.now().year, DateTime.now().month - 3,
+                DateTime.now().day),
+            endDay: DateTime(DateTime.now().year, DateTime.now().month + 3,
+                DateTime.now().day),
+            initialSelectedDay: DateTime.now(),
+            initialCalendarFormat: CalendarFormat.twoWeeks,
+            startingDayOfWeek: StartingDayOfWeek.monday,
+            calendarController: _calendarController, //required
+            locale: 'en_US',
+            onDaySelected: (date, events, e) {
+              setState(() {
+                print({'object', events, e});
+                selectedEvents = events;
+              });
+            },
+            builders: CalendarBuilders(
+              selectedDayBuilder: (context, date, events) => Container(
+                margin: const EdgeInsets.all(4.0),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(25.0),
+                ),
+                child: Text(
+                  date.day.toString(),
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              todayDayBuilder: (context, date, events) => Container(
+                margin: const EdgeInsets.all(4.0),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(25.0),
+                ),
+                child: Text(
+                  date.day.toString(),
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ),
+          ...selectedEvents.isEmpty
+              ? {
+                  const Text('No Events on the selected day'),
+                }
+              : selectedEvents.map(
+                  (event) => Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Card(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          const ListTile(
+                            leading: Icon(Icons.album),
+                            title: Text('The Enchanted Nightingale'),
+                            subtitle: Text(
+                                'Music by Julie Gable. Lyrics by Sidney Stein.'),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: <Widget>[
+                              TextButton(
+                                child: const Text('BUY TICKETS'),
+                                onPressed: () {/* ... */},
+                              ),
+                              const SizedBox(width: 8),
+                              TextButton(
+                                child: const Text('LISTEN'),
+                                onPressed: () {/* ... */},
+                              ),
+                              const SizedBox(width: 8),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+        ],
+      ),
     );
+  }
+
+  _showAddDialog() async {
+    await showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              backgroundColor: Colors.white70,
+              title: Text("Add Events"),
+              content: TextField(
+                controller: _eventController,
+              ),
+              actions: <Widget>[
+                FlatButton(
+                  child: const Text(
+                    "Save",
+                    style: TextStyle(
+                        color: Colors.green, fontWeight: FontWeight.bold),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      if (events[_calendarController.selectedDay] != null) {
+                        events[_calendarController.selectedDay]
+                            ?.add(Event(_eventController.text));
+                      } else {
+                        events[_calendarController.selectedDay] = [
+                          Event(_eventController.text)
+                        ];
+                      }
+                      _eventController.clear();
+                      Navigator.pop(context);
+                    });
+                  },
+                )
+              ],
+            ));
   }
 }
